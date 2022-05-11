e7e6fe802203f80937609d2cb3d48dfeca093454
Davi <obrigatoriopcional@gmail.com>
2022-05-11 16:40:23 -0300

    v1.1 pequenas correcoes

---
+++ assets/js/poems.js
@@ -0,0 +1,188 @@
+let poemsList = [
+  {
+    archive: "poemas/poem_001.txt",
+    name: "Amor é fogo que arde sem se ver",
+  },
+  {
+    archive: "poemas/poem_002.txt",
+    name: "Verdes são os campos",
+  },
+  {
+    archive: "poemas/poem_003.txt",
+    name: "Transforma-se o amador na cousa amada",
+  },
+  {
+    archive: "poemas/poem_004.txt",
+    name: "Se tanta pena tenho merecida",
+  },
+  {
+    archive: "poemas/poem_005.txt",
+    name: "Busque Amor novas artes, novo engenho",
+  },
+  {
+    archive: "poemas/poem_006.txt",
+    name: "Enquanto quis Fortuna que tivesse",
+  },
+  {
+    archive: "poemas/poem_007.txt",
+    name: "Tomou-me vossa vista soberana",
+  },
+  {
+    archive: "poemas/poem_008.txt",
+    name: "Quem pode livre ser, gentil Senhora",
+  },
+  {
+    archive: "poemas/poem_009.txt",
+    name: "O fogo que na branda cera ardia",
+  },
+  {
+    archive: "poemas/poem_010.txt",
+    name: "Tanto de meu estado me acho incerto",
+  },
+  {
+    archive: "poemas/poem_011.txt",
+    name: "Alma minha gentil, que te partiste",
+  },
+  {
+    archive: "poemas/poem_012.txt",
+    name: "Quando de minhas mágoas a comprida",
+  },
+  {
+    archive: "poemas/poem_013.txt",
+    name: "Ah! minha Dinamene! Assim deixaste",
+  },
+  {
+    archive: "poemas/poem_014.txt",
+    name: "Endechas a Bárbara escrava",
+  },
+  {
+    archive: "poemas/poem_015.txt",
+    name: "Descalça vai pera a fonte",
+  },
+  {
+    archive: "poemas/poem_016.txt",
+    name: "Perdigão perdeu a pena",
+  },
+  {
+    archive: "poemas/poem_017.txt",
+    name: "Mudam-se os tempos, mudam-se as vontades",
+  },
+  {
+    archive: "poemas/poem_018.txt",
+    name: "No mundo quis o Tempo que se achasse",
+  },
+  {
+    archive: "poemas/poem_019.txt",
+    name: "Quando me quer enganar",
+  },
+  {
+    archive: "poemas/poem_020.txt",
+    name: "Amor, que o gesto humano na alma escreve",
+  },
+  {
+    archive: "poemas/poem_021.txt",
+    name: "Quem presumir, Senhora, de louvar-vos",
+  },
+  {
+    archive: "poemas/poem_022.txt",
+    name: "Posto me tem Fortuna em tal estado",
+  },
+  {
+    archive: "poemas/poem_023.txt",
+    name: "Ao desconcerto do Mundo",
+  },
+  {
+    archive: "poemas/poem_024.txt",
+    name: "Eu cantarei de amor tão docemente(13/2/95)",
+  },
+  {
+    archive: "poemas/poem_025.txt",
+    name: "Que me quereis, perpétuas saudades?(20/2/95)",
+  },
+  {
+    archive: "poemas/poem_026.txt",
+    name: "Se as penas com que Amor tão mal me trata (6/3/95)",
+  },
+  {
+    archive: "poemas/poem_027.txt",
+    name: "Se me vem tanta glória só de olhar-te (22/5/95)",
+  },
+  {
+    archive: "poemas/poem_028.txt",
+    name: "Quem vê, Senhora, claro e manifesto (31/7/95)",
+  },
+  {
+    archive: "poemas/poem_029.txt",
+    name: "O dia em que nasci moura e pereça (5/2/96)",
+  },
+  {
+    archive: "poemas/poem_030.txt",
+    name: "Julga-me a gente toda por perdido (14/2/96)",
+  },
+  {
+    archive: "poemas/poem_031.txt",
+    name: "Vencido está de amor (14/2/96)",
+  },
+  {
+    archive: "poemas/poem_032.txt",
+    name: "Senhora minha, se de pura inveja (28/10/96)",
+  },
+  {
+    archive: "poemas/poem_033.txt",
+    name: "O cisne, quando sente ser chegada (28/4/97)",
+  },
+  {
+    archive: "poemas/poem_034.txt",
+    name: "Se pena por amar-vos se merece (13/10/97)",
+  },
+  {
+    archive: "poemas/poem_035.txt",
+    name: "Sempre a Razão vencida foi de Amor (11/5/98)",
+  },
+  {
+    archive: "poemas/poem_036.txt",
+    name: "Coitado! que em um tempo choro e rio (22/6/98)",
+  },
+  {
+    archive: "poemas/poem_037.txt",
+    name: "Lembranças, que lembrais meu bem passado (21/9/98)",
+  },
+  {
+    archive: "poemas/poem_038.txt",
+    name: "Nunca em amor danou o atrevimento (1/3/99)",
+  },
+  {
+    archive: "poemas/poem_039.txt",
+    name: "Erros meus, má fortuna, amor ardente (22/3/99)",
+  },
+  {
+    archive: "poemas/poem_040.txt",
+    name: "Qual tem a borboleta por costume (12/4/99)",
+  },
+  {
+    archive: "poemas/poem_041.txt",
+    name: "O tempo acaba o ano, o mês e a hora (7/6/99)",
+  },
+  {
+    archive: "poemas/poem_042.txt",
+    name: "Quem diz que Amor é falso ou enganoso (10/1/00)",
+  },
+  {
+    archive: "poemas/poem_043.txt",
+    name: "De quantas graças tinha, a Natureza (17/1/00)",
+  },
+  {
+    archive: "poemas/poem_044.txt",
+    name: "Ditoso seja aquele que somente (20/3/00)",
+  },
+  {
+    archive: "poemas/poem_045.txt",
+    name: "Se só no ver puramente (15/5/00)",
+  },
+  {
+    archive: "poemas/poem_046.txt",
+    name: "Onde acharei lugar tão apartado",
+  },
+];
+
+export default poemsList;
